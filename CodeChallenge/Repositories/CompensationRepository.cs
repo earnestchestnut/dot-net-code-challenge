5f55ace6e537cb98ba4164bf25c23392b1cc34e6
Earnest Chestnut Jr <earnest@dayslice.io>
2023-09-28 00:24:06 -0400

    Add Compensation Repository

---
+++ CodeChallenge/Repositories/CompensationRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeChallenge.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using CodeChallenge.Data;
+
+namespace CodeChallenge.Repositories
+{
+    public class CompensationRepository : ICompensationRepository
+    {
+        private readonly CompensationContext _compensationContext;
+        private readonly ILogger<ICompensationRepository> _logger;
+
+        public CompensationRepository(ILogger<ICompensationRepository> logger, CompensationContext compensationContext)
+        {
+            _compensationContext = compensationContext;
+            _logger = logger;
+        }
+
+
+        public Compensation Add(Compensation compensation)
+        {
+            _compensationContext.Add(compensation);
+            return compensation;
+        }
+
+        public Compensation GetByEmployeeId(string employeeId)
+        {
+            return _compensationContext.Compensations
+                .Where(c => c.Employee.EmployeeId == employeeId)
+                .Include(e => e.Employee)
+                .ThenInclude(e => e.DirectReports)
+                .ThenInclude(e => e.DirectReports)
+                .SingleOrDefault();
+        }
+
+        public Task SaveAsync()
+        {
+            return _compensationContext.SaveChangesAsync();
+        }
+    }
+}
+
