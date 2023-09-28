d4894c312464ef79d9b332a1774ee1c3d697e1c8
Earnest Chestnut Jr <earnest@dayslice.io>
2023-09-28 00:23:39 -0400

    Add Compensation Repository interface

---
+++ CodeChallenge/Repositories/ICompensationRepository.cs
@@ -0,0 +1,14 @@
+﻿using CodeChallenge.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.Repositories
+{
+	public interface ICompensationRepository
+	{
+		Compensation Add(Compensation compensation);
+		Compensation GetByEmployeeId(string employeeId);
+        Task SaveAsync();
+    }
+}
+
