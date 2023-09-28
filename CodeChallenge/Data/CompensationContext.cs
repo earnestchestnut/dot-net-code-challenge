367c47bcdf850bdccb62be1e1297c4b1f0121f52
Earnest Chestnut Jr <earnest@dayslice.io>
2023-09-28 00:21:52 -0400

    Add Compensation Context

---
+++ CodeChallenge/Data/CompensationContext.cs
@@ -0,0 +1,24 @@
+﻿using CodeChallenge.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CodeChallenge.Data
+{
+	public class CompensationContext : DbContext
+	{
+		public CompensationContext(DbContextOptions<CompensationContext> options) : base(options)
+		{
+
+		}
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Compensation type is [Keyless]
+            // Add key on model create to avoid: Unable to track an instance of type because it does not have a primary key
+            modelBuilder.Entity<Compensation>().HasKey(c => new { c.Effectivedate, c.Salary });
+
+        }
+
+        public DbSet<Compensation> Compensations { get; set; }
+
+    }
+}
