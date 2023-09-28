b3e903b34873ec83aef9779acf575ed186cc7f3a
Earnest Chestnut Jr <earnest@dayslice.io>
2023-09-28 00:21:18 -0400

    Add Compensation model

---
+++ CodeChallenge/Models/Compensation.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CodeChallenge.Models
+{
+	[Keyless]
+	public class Compensation
+    {
+		public Employee Employee { get; set; }
+		public long Salary { get; set; } = 0;
+		public DateTime Effectivedate { get; set; } = DateTime.UtcNow;
+    }
+}
+
